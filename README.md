93a27cdb29cb50fa23f76519649f950d25db3307
jorovipe97 <jorovipe@gmail.com>
2017-07-09 19:46:56 -0500

    Readme updated
    Readme credits and links were updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# surface_water_simulation
+# Surface water simulation
 This is a simple surface ocean water simulation based on a sum of sine functions approach to calculate a height map and get dynamically water surface orientation from GPU in a vertex shader.
+Visualization characteristics are provided by Jeremy Bouny water material project for ThreeJS [@jbouny/ocean](https://github.com/jbouny/ocean). 
+This Project could be considered a basic implementation of GPU gems chapter 1, named [Effective Water Simulation from Physical Models](https://developer.nvidia.com/gpugems/GPUGems/gpugems_ch01.html) By Mark Finch and Cyan Worlds.
---
+++ assets/js/demo.js
@@ -0,0 +1,116 @@
+var DEMO = {
+	ms_Canvas: null,
+	ms_Renderer: null,
+	ms_Camera: null, 
+	ms_Scene: null, 
+	ms_Controls: null,
+	ms_Water: null,
+
+    enable: (function enable() {
+        try {
+            var aCanvas = document.createElement('canvas');
+            return !! window.WebGLRenderingContext && (aCanvas.getContext('webgl') || aCanvas.getContext('experimental-webgl'));
+        }
+        catch(e) {
+            return false;
+        }
+    })(),
+	
+	initialize: function initialize(inIdCanvas) {
+		this.ms_Canvas = $('#' + inIdCanvas);
+		//this.ms_Canvas = document.getElementById(inIdCanvas);
+		
+		// Initialize Renderer, Camera and Scene
+		this.ms_Renderer = this.enable? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+		this.ms_Canvas.html(this.ms_Renderer.domElement);
+		this.ms_Scene = new THREE.Scene();
+		
+		this.ms_Camera = new THREE.PerspectiveCamera(55.0, WINDOW.ms_Width / WINDOW.ms_Height, 0.5, 3000000);
+		this.ms_Camera.position.set(1000, 500, -1500);
+		this.ms_Camera.lookAt(new THREE.Vector3(0, 0, 0));
+		
+		// Initialize Orbit control		
+		this.ms_Controls = new THREE.OrbitControls(this.ms_Camera, this.ms_Renderer.domElement);
+	
+		// Add light
+		var directionalLight = new THREE.DirectionalLight(0xffff55, 1);
+		directionalLight.position.set(-600, 300, 600);
+		this.ms_Scene.add(directionalLight);
+		
+		// Load textures		
+		var waterNormals = new THREE.ImageUtils.loadTexture('../assets/img/waternormals.jpg');
+		waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping; 
+		
+		// Create the water effect
+		this.ms_Water = new THREE.Water(this.ms_Renderer, this.ms_Camera, this.ms_Scene, {
+			textureWidth: 256,
+			textureHeight: 256,
+			waterNormals: waterNormals,
+			alpha: 	.85,
+			sunDirection: directionalLight.position.normalize(),
+			sunColor: 0xffffff,
+			waterColor: 0x7e9ec7,
+			betaVersion: 0,
+			side: THREE.DoubleSide
+		});
+		var aMeshMirror = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(2000, 2000, 10, 10), 
+			this.ms_Water.material
+		);
+		aMeshMirror.add(this.ms_Water);
+		aMeshMirror.rotation.x = - Math.PI * 0.5;
+		
+		this.ms_Scene.add(aMeshMirror);
+	
+		this.loadSkyBox();
+	},
+	
+	loadSkyBox: function loadSkyBox() {
+		var aCubeMap = THREE.ImageUtils.loadTextureCube([
+		  '../assets/img/px2.jpg',
+		  '../assets/img/nx2.jpg',
+		  '../assets/img/py2.jpg',
+		  '../assets/img/ny2.jpg',
+		  '../assets/img/pz2.jpg',
+		  '../assets/img/nz2.jpg'
+		]);
+		aCubeMap.format = THREE.RGBFormat;
+
+		var aShader = THREE.ShaderLib['cube'];
+		aShader.uniforms['tCube'].value = aCubeMap;
+
+		var aSkyBoxMaterial = new THREE.ShaderMaterial({
+		  fragmentShader: aShader.fragmentShader,
+		  vertexShader: aShader.vertexShader,
+		  uniforms: aShader.uniforms,
+		  depthWrite: false,
+		  side: THREE.BackSide
+		});
+
+		var aSkybox = new THREE.Mesh(
+		  new THREE.BoxGeometry(1000000, 1000000, 1000000),
+		  aSkyBoxMaterial
+		);
+		
+		this.ms_Scene.add(aSkybox);
+	},
+
+    display: function display() {
+		this.ms_Water.render();
+		this.ms_Renderer.render(this.ms_Scene, this.ms_Camera);
+	},
+	
+	update: function update() {
+		this.ms_Water.material.uniforms.time.value += 1.0 / 60.0;
+		this.ms_Controls.update();
+		this.display();
+	},
+	
+	resize: function resize(inWidth, inHeight) {
+		this.ms_Camera.aspect =  inWidth / inHeight;
+		this.ms_Camera.updateProjectionMatrix();
+		this.ms_Renderer.setSize(inWidth, inHeight);
+		this.ms_Canvas.html(this.ms_Renderer.domElement);
+		this.display();
+	}
+};
